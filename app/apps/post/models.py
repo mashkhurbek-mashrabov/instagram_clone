a2a7fe4626b3cdafc4d4abd6be039f8fe1b4b579
Mashhurbek Mashrabov <mashkhurbek.mashrabov@gmail.com>
2024-01-24 15:27:19 +0500

    post is in the progress

--- app/apps/post/models.py
+++ app/apps/post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from common.models import BaseModel
+from django.contrib.auth import get_user_model
+from django.core.validators import FileExtensionValidator, MaxLengthValidator
 
-# Create your models here.
+User = get_user_model()
+
+class Post(BaseModel):
+    auth = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/', 
+                              validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
+    caption = models.TextField(validators=[MaxLengthValidator(2000)])
--- scripts/run.sh
+++ scripts/run.sh
@@ -15,4 +15,3 @@ python manage.py wait_for_db
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-
